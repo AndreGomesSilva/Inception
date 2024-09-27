c783f84283aa2cc25915249fa083bb20812ec3fa
AndreGomesSilva <silva_andregomes@outlook.com>
2024-09-26 21:30:34 -0300

    WIP: script to wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 wp --allow-root config create \
   --dbname="$DB_NAME" \
   --dbuser="$WP_ADMIN_USR" \
@@ -10,18 +12,20 @@ wp --allow-root config create \
 wp core install --allow-root \
   --path=/var/www/wordpress \
   --title="$WP_TITLE" \
-  --url=$DOMAIN_NAME \
-  --admin_user=$WP_ADMIN_USR \
-  --admin_password=$WP_ADMIN_PWD \
-  --admin_email=$WP_ADMIN_EMAIL
+  --url="$DOMAIN_NAME" \
+  --admin_user="$WP_ADMIN_USR" \
+  --admin_password="$WP_ADMIN_PWD" \
+  --admin_email="$WP_ADMIN_EMAIL"
 
 wp user create --allow-root \
   --path=/var/www/wordpress \
   "$WP_USR" "$WP_EMAIL" \
-  --user_pass=$WP_PWD \
+  --user_pass="$WP_PWD" \
   --role='author'
 
 #wp theme install astra --activate --allow-root
-wp --allow-root theme activate twentytwentytwo
+wp --allow-root theme activate twentytwentytwo --path=/var/www/wordpress
+
+fuser -k 9000/tcp || true
 
 exec php-fpm7.4 -F
