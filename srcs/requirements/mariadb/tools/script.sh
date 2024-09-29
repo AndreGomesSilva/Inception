4938a9948ab695a6f901c6568fe624ea72e546d4
AndreGomesSilva <silva_andregomes@outlook.com>
2024-09-29 14:52:27 -0300

    add: makefile alias for localhost

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,11 +2,8 @@
 
 service mariadb start
 
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-
-mariadb -u root -e "CREATE USER IF NOT EXISTS '$WP_ADMIN_USR'@'%' IDENTIFIED BY '$WP_ADMIN_PWD';"
-
-mariadb -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$WP_ADMIN_USR'@'%' IDENTIFIED BY '$WP_ADMIN_PWD';"
-
-mariadb -u root -e "FLUSH PRIVILEGES;"
-
+mariadb -u root -e \
+  "CREATE DATABASE IF NOT EXISTS ${DB_NAME}; \
+  CREATE USER '${ADMIN_USR}'@'%' IDENTIFIED BY '${ADMIN_PWD}'; \
+  GRANT ALL ON ${DB_NAME}.* TO '${ADMIN_USR}'@'%' IDENTIFIED BY '${ADMIN_PWD}'; \
+  FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,31 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+wp --allow-root config create --dbname="$DB_NAME" --dbuser="$ADMIN_USR" --dbpass="$ADMIN_PWD"  --dbhost=mariadb --dbprefix="wp_"
 
-wp --allow-root config create \
-  --dbname="$DB_NAME" \
-  --dbuser="$WP_ADMIN_USR" \
-  --dbpass="$WP_ADMIN_PWD" \
-  --dbhost=mariadb \
-  --dbprefix="wp_"
+wp core install --allow-root --path=/var/www/html --title="$TITLE" --url=$DOMAIN_NAME --admin_user=$ADMIN_USR --admin_password=$ADMIN_PWD --admin_email=$ADMIN_EMAIL
 
-wp core install --allow-root \
-  --path=/var/www/wordpress \
-  --title="$WP_TITLE" \
-  --url="$DOMAIN_NAME" \
-  --admin_user="$WP_ADMIN_USR" \
-  --admin_password="$WP_ADMIN_PWD" \
-  --admin_email="$WP_ADMIN_EMAIL"
+wp user create --allow-root --path=/var/www/html "$USR_NAME" "$USR_EMAIL" --user_pass=$USR_PWD  --role='author'
 
-wp user create --allow-root \
-  --path=/var/www/wordpress \
-  "$WP_USR" "$WP_EMAIL" \
-  --user_pass="$WP_PWD" \
-  --role='author'
-
-#wp theme install astra --activate --allow-root
-wp --allow-root theme activate twentytwentytwo --path=/var/www/wordpress
-
-fuser -k 9000/tcp || true
+wp --allow-root theme activate twentytwentyfour
 
 exec php-fpm7.4 -F
