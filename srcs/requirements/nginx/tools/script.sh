246115900d4f2d57410a57f0ea1a24eea56092ef
AndreGomesSilva <silva_andregomes@outlook.com>
2024-10-02 21:30:09 -0300

    change restar mode on compose to alway

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+CONFIG_FILE="/etc/nginx/conf.d/default.conf"
 
-envsubst '${CERTS_KEY} ${CERTS_CRT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+# Check if the config file already exists and contains the correct certificate paths
+if grep -q "${CERTS_KEY}" "${CONFIG_FILE}" && grep -q "${CERTS_CRT}" "${CONFIG_FILE}"; then
+    echo "Nginx configuration already contains the certificate paths. Skipping envsubst."
+else
+    echo "Nginx configuration does not exist or does not contain the certificate paths. Running envsubst."
+    envsubst '${CERTS_KEY} ${CERTS_CRT}' < /etc/nginx/conf.d/nginx.conf.template > "${CONFIG_FILE}"
+fi
 
 nginx -g 'daemon off;'
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-
+if [ ! -f /var/www/html/wp-config.php ]; then
 wp --allow-root config create --dbname="$DB_NAME" --dbuser="$ADMIN_USR" --dbpass="$ADMIN_PWD"  --dbhost=mariadb --dbprefix="wp_"
 
 wp core install --allow-root --path=/var/www/html --title="$TITLE" --url=$DOMAIN_NAME --admin_user=$ADMIN_USR --admin_password=$ADMIN_PWD --admin_email=$ADMIN_EMAIL
 
 wp user create --allow-root --path=/var/www/html "$USR_NAME" "$USR_EMAIL" --user_pass=$USR_PWD  --role='author'
 
-wp --allow-root theme install astra
-wp theme activate astra --allow-root
+wp --allow-root theme install twentytwentytwo --activate
+fi
 
 exec php-fpm7.4 -F
